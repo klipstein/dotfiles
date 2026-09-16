6f325b80776fd3423a1ddb469852d5a0b13d4c48
Tobias von Klipstein <tobias@klpstn.com>
2026-09-16 14:50:06 +0200

    Do not open .html with zed

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/kli0002t/.docker/bin"
+# End of Docker Desktop section.
+
 set -x -g LS_COLORS "di=38;5;27:fi=38;5;7:ln=38;5;51:pi=40;38;5;11:so=38;5;13:or=38;5;197:mi=38;5;161:ex=38;5;9:"
 
 set -x -g TERM "xterm-256color"

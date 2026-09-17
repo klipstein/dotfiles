1dc27497689ad85a31c8e2d2c37685265cf4e181
Tobias von Klipstein <tobias@klpstn.com>
2026-09-17 14:31:42 +0200

    Fix things

--- repos/setup.sh
+++ repos/setup.sh
@@ -1,35 +1,38 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-DIR=$(dirname "$0")
-cd "$DIR"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+cd "$DIR" || exit 1
 
 . ../scripts/functions.sh
 
 COMMENT=\#*
 REPO_PATH="$HOME/repos"
 
-find * -name "*.list" | while read fn; do
-    folder="${fn%.*}"
+find . -name "*.list" | sort | while read -r fn; do
+    folder="${fn#./}"
+    folder="${folder%.list}"
 
     info "Cloning $folder repositories..."
-
-    substep_info "Creating $folder folder..."
     mkdir -p "$REPO_PATH/$folder"
-    while read repo; do
+
+    while read -r repo || [ -n "$repo" ]; do
         if [[ $repo == $COMMENT || -z "$repo" ]]; then
             continue
         fi
-        pushd "$REPO_PATH/$folder" &> /dev/null
-        git clone $repo &> /dev/null
-        res=$?
-        if [[ $res -eq 128 ]]; then
-            substep_success "$repo already exists."
-        elif [[ $res -eq 0 ]]; then
-            substep_success "Cloned $repo."
+
+        repo_name=$(basename "$repo" .git)
+        target_dir="$REPO_PATH/$folder/$repo_name"
+
+        if [ -d "$target_dir/.git" ]; then
+            substep_success "$repo already exists at $target_dir."
         else
-            substep_error "Failed to clone $repo."
+            substep_info "Cloning $repo into $target_dir..."
+            if git clone "$repo" "$target_dir"; then
+                substep_success "Cloned $repo."
+            else
+                substep_error "Failed to clone $repo. Check your SSH keys or network access."
+            fi
         fi
-        popd &> /dev/null
     done < "$fn"
     success "Finished cloning $folder repositories."
 done
